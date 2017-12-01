51d45c0649c904581411add5a3bd0935af56dcba
Dam Viet <vietdien2005@gmail.com>
2017-12-01 17:47:04 +0700

    fix error testing scripts

--- tests/test_kafka.sh
+++ tests/test_kafka.sh
@@ -5,12 +5,12 @@ set -e
 CMD=(
 	'kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1  --partitions 1 --topic testing --if-not-exists'
 	'kafka-topics.sh --list --zookeeper zookeeper:2181'
-	# 'kafka-topics.sh --delete --zookeeper zookeeper:2181 --topic testing'
+	'kafka-topics.sh --delete --zookeeper zookeeper:2181 --topic testing'
 )
 
 for i in "${CMD[@]}"
 do
 	echo $i
-	eval 'docker exec -it testkafka_kafka_1' $i 
+	eval 'docker exec -it kafka' $i 
 	echo '======================================================'
 done
